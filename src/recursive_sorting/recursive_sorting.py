fffd4b5cfd53a55ec0cd07778c184c99c95171d5
Ryan Ansberry <ansman92@gmail.com>
2020-02-28 17:48:00 -0800

    Recursive Sort: merg_sort() completed

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -34,6 +34,14 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # Base Case:
+    if len( arr ) > 1:
+        # Continuously divide until single item arrays (recursion)
+        left = merge_sort( arr[ 0 : len( arr ) // 2 ])
+        right = merge_sort( arr[ len( arr ) // 2 : ])
+
+        # Compare values and sort into arrays
+        arr = merge( left, right )
 
     return arr
 
